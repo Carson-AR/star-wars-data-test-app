2b2c076f5ed94cf6fb03859e18dbe1dd823d2a41
Carson-AR <Carsonarr@icloud.com>
2024-06-28 11:23:36 +0100

    add space ships

--- __main__.py
+++ __main__.py
@@ -1,15 +1,19 @@
 from questoin import question
 from data import fetch_people
 from utilities import find_person
+from data import fetch_space_ship
+from utilities import find_space_ship
+from questoin import ship_question
 
-
-whos_data = " "
-
-
+name = input("what person or ship would you like")
 # question()
 people = fetch_people()
-persons_name = input(
-    "whos data would you like to see? (with no spaces between words, no caps)"
-)
-person = find_person(people, persons_name)
-print(person)
+person = find_person(people, name)
+if person is not None:
+    print(person)
+else:
+    space_ships = fetch_space_ship()
+    space_ship = find_space_ship(space_ships, name)
+    print(space_ship)
+
+#
--- utilities.py
+++ utilities.py
@@ -7,3 +7,9 @@ def find_person(people, persons_name):
         print(
             "either we do not have that charactar or you did not enter a valid name. please try again"
         )
+
+
+def find_space_ship(space_ships, ship_name):
+    for space_ship in space_ships:
+        if ship_name.lower() in space_ship["name"].lower():
+            return space_ship
